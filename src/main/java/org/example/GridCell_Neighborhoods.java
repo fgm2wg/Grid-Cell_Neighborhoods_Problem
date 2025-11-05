d50410838b7d3770eff5ec45f6e364ed8e8e1a3f
fgm2wg <fgm2wg@virginia.edu>
2025-11-05 14:45:52 -0500

    Completed the problem successfully - Implemented GridCell_Neighborhoods class with solver and helper methods to solve the problem - Implemented functions in Main.java to help run and visualize the results - Now need to add JUnit tests and README.md

---
+++ src/main/java/org/example/GridCell_Neighborhoods.java
@@ -0,0 +1,147 @@
+/**
+ * NOTE: I did use these resources as reference to help get an idea of how best to solve the problem and implement BFS algorithm.
+ * I hope that it is okay that I used outside resources since I would normally use this approach when solving a new problem.
+ * Sources:
+ * https://www.geeksforgeeks.org/dsa/breadth-first-traversal-bfs-on-a-2d-array/
+ * - Found how to best implement the BFS algorithm in Java.
+ * https://stackoverflow.com/questions/77274736/how-to-find-neighbors-in-a-grid
+ * - Found how to best find adjacent neighbors in grid efficiently.
+ */
+
+package org.example;
+
+import java.util.*;
+
+public class GridCell_Neighborhoods {
+    // --- Private class fields ---
+    private final ArrayList<ArrayList<Integer>> grid;
+    private ArrayList<ArrayList<Boolean>> visited_grid;
+    private final int n, numRows, numCols;
+
+    // --- Public class methods ---
+
+    /**
+     * Constructor for the class to setup the grid, N, and grid dimensions.
+     *
+     * @param grid The n x m grid of integers.
+     * @param n    The number of steps to count from each positive number in the grid.
+     */
+    public GridCell_Neighborhoods(ArrayList<ArrayList<Integer>> grid, int n) {
+        this.grid = grid;
+        this.n = n;
+
+        // Get the number of rows and columns in the grid
+        this.numRows = grid.size();
+        this.numCols = grid.getFirst().size();
+
+        // Generate a matching size grid for the nodes visited all initialized to false
+        generateVisitedGrid();
+    }
+
+    /**
+     * Function to count the number of cells within N steps of positive numbers via a BFS approach.
+     * Should probably separate out pieces like the BFS portion to separate functions, but kept like this for simplicity.
+     *
+     * @return The total number of cells within N steps of positive numbers in the grid.
+     */
+    public int numCellsWithinNSteps() {
+        // Declare a queue to hold the values to visit in the grid for BFS
+        Queue<ArrayList<Integer>> queue = new LinkedList<>();
+
+        // Iterate over the entire grid
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                // If positive number found, then add its location and distance initialized to 0 to the queue and mark node as visited
+                if (grid.get(i).get(j) > 0) {
+                    queue.add(new ArrayList<>(Arrays.asList(i, j, 0)));
+                    visited_grid.get(i).set(j, true);
+                }
+            }
+        }
+
+        // Create the direction offsets we need to find 4 adjacent neighboring cells
+        List<List<Integer>> directions = List.of(Arrays.asList(-1, 0), Arrays.asList(0, -1), Arrays.asList(1, 0), Arrays.asList(0, 1));
+
+        // Perform BFS for the positive values in the queue
+        while (!queue.isEmpty()) {
+            // Dequeue the list of values for the positive integer and split into coordinates and distance
+            ArrayList<Integer> values = queue.poll();
+            int i = values.get(0), j = values.get(1), dist = values.get(2);
+
+            // If the distance is equal to number of steps we can take, then skip it
+            if (dist == n)
+                continue;
+
+            // Iterate over the four directions to find adjacent cells
+            for (List<Integer> direction : directions) {
+                // Get and apply the offsets to find the neighboring indices
+                int offset_i = direction.get(0), offset_j = direction.get(1);
+                int neighbor_i = i + offset_i, neighbor_j = j + offset_j;
+                // If the neighbor has valid coordinates and has not been visited, then mark as visited and add it to the queue
+                if (isNeighborValid(neighbor_i, neighbor_j) && !visited_grid.get(neighbor_i).get(neighbor_j)) {
+                    visited_grid.get(neighbor_i).set(neighbor_j, true);
+                    queue.add(new ArrayList<>(Arrays.asList(neighbor_i, neighbor_j, dist + 1)));
+                }
+            }
+        }
+
+        // Initialize count of cells to 0
+        int count = 0;
+
+        // Iterate over entire grid
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                // If cell is marked as visited, then increment the count
+                if (visited_grid.get(i).get(j))
+                    count++;
+            }
+        }
+
+        // Return the final count
+        return count;
+    }
+
+    /**
+     * Function to generate the grid to store whether cells were visited or not in BFS.
+     *
+     * @return ArrayList<ArrayList < Boolean>>, a grid of equal dimensions to input grid with boolean values all initialized to false.
+     */
+    public ArrayList<ArrayList<Boolean>> generateVisitedGrid() {
+        // Initialize the visited grid
+        visited_grid = new ArrayList<>();
+
+        // Construct the grid to be the same size as the input grid
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Boolean> row = new ArrayList<>();
+
+            // Initialize all values to false
+            for (int j = 0; j < numCols; j++)
+                row.add(false);
+
+            visited_grid.add(row);
+        }
+
+        // Return the final constructed visited grid
+        return visited_grid;
+    }
+
+    /**
+     * Function to check if a neighbor's coordinates are valid within the grid bounds.
+     *
+     * @param neighbor_i The row coordinate for the neighbor.
+     * @param neighbor_j The column coordinate for the neighbor.
+     * @return Boolean, true if neighbor is valid; false otherwise.
+     */
+    public Boolean isNeighborValid(int neighbor_i, int neighbor_j) {
+        // True if i within [0, numRows-1] and j within [0, numCols-1]
+        return neighbor_i >= 0 && neighbor_i < numRows && neighbor_j >= 0 && neighbor_j < numCols;
+    }
+
+    /**
+     * Returns the visited_grid.
+     * @return ArrayList<ArrayList<Boolean>>, the visited_grid.
+     */
+    public ArrayList<ArrayList<Boolean>> getVisitedGrid() {
+        return visited_grid;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,150 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
+    /**
+     * Main function to run the example scenario.
+     *
+     * @param args Command-line arguments.
+     */
     public static void main(String[] args) {
-        System.out.println("Test");
+        // --- Run Example 1: One positive cell fully contained; N=3 ---
+        List<List<Integer>> pos_val_coords = List.of(Arrays.asList(5, 5)); // (row, col), 1-indexed
+        ArrayList<ArrayList<Integer>> grid = generateGrid(11, 11, pos_val_coords);
+        int n = 3;
+
+        GridCell_Neighborhoods solver = new GridCell_Neighborhoods(grid, n);
+        int result = solver.numCellsWithinNSteps();
+        System.out.println("Result: " + result);
+        ArrayList<ArrayList<Boolean>> visited_grid = solver.getVisitedGrid();
+        printVisitedGrid(grid, visited_grid);
+        System.out.println();
+
+        // --- Run Example 2: One positive cell near an edge; N=3 ---
+        pos_val_coords = List.of(Arrays.asList(5, 1)); // (row, col), 1-indexed
+        grid = generateGrid(11, 11, pos_val_coords);
+
+        solver = new GridCell_Neighborhoods(grid, n);
+        result = solver.numCellsWithinNSteps();
+        System.out.println("Result: " + result);
+        visited_grid = solver.getVisitedGrid();
+        printVisitedGrid(grid, visited_grid);
+        System.out.println();
+
+        // --- Run Example 3: Two positive values with disjoint neighborhoods; N=2 ---
+        pos_val_coords = List.of(Arrays.asList(3, 7), Arrays.asList(7, 3)); // (row, col), 1-indexed
+        grid = generateGrid(11, 11, pos_val_coords);
+        n = 2;
+
+        solver = new GridCell_Neighborhoods(grid, n);
+        result = solver.numCellsWithinNSteps();
+        System.out.println("Result: " + result);
+        visited_grid = solver.getVisitedGrid();
+        printVisitedGrid(grid, visited_grid);
+        System.out.println();
+
+        // --- Run Example 4: Two positive values with overlapping neighborhoods; N=2 ---
+        pos_val_coords = List.of(Arrays.asList(7, 3), Arrays.asList(6, 5)); // (row, col), 1-indexed
+        grid = generateGrid(11, 11, pos_val_coords);
+
+        solver = new GridCell_Neighborhoods(grid, n);
+        result = solver.numCellsWithinNSteps();
+        System.out.println("Result: " + result);
+        visited_grid = solver.getVisitedGrid();
+        printVisitedGrid(grid, visited_grid);
+        System.out.println();
+    }
+
+    /**
+     * Function to generate a random grid with n rows x m columns of values ranging from -10 to 10.
+     *
+     * @param numRows The number of rows to make the grid have.
+     * @param numCols The number of columns to make the grid have.
+     * @return n x m grid of random integers between -10 and 10.
+     */
+    public static ArrayList<ArrayList<Integer>> generateRandomGrid(int numRows, int numCols) {
+        // Initialize empty grid and random number generator
+        ArrayList<ArrayList<Integer>> grid = new ArrayList<>();
+        Random random = new Random();
+
+        // Iterate over all rows and columns adding a random number between -10 and 10 to the grid
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            for (int j = 0; j < numCols; j++)
+                row.add(random.nextInt(-10, 10));
+            grid.add(row);
+        }
+
+        // Return the constructed grid
+        return grid;
+    }
+
+    /**
+     * Function to generate an n x m grid of negative values except for the coordinates specified to be positive.
+     *
+     * @param numRows        The number of rows to make the grid have.
+     * @param numCols        The number of columns to make the grid have.
+     * @param pos_val_coords The list of row, column 1-indexed coordinates to place positive numbers.
+     * @return An n x m grid of values.
+     */
+    public static ArrayList<ArrayList<Integer>> generateGrid(int numRows, int numCols, List<List<Integer>> pos_val_coords) {
+        // Initialize empty grid
+        ArrayList<ArrayList<Integer>> grid = new ArrayList<>();
+
+        // Iterate over all rows and columns adding -1 to all cells
+        for (int i = 0; i < numRows; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j < numCols; j++)
+                row.add(-1);
+
+            grid.add(row);
+        }
+
+        // Iterate over the positive value coordinates given and set those cells to have the value 1
+        for (List<Integer> coord : pos_val_coords) {
+            int i = coord.get(0), j = coord.get(1);
+            grid.get(i).set(j, 1);
+        }
+
+        // Return the constructed grid
+        return grid;
+    }
+
+    /**
+     * Print out the visualized graph with positive numbers being green and all visited neighbors as blue.
+     *
+     * @param grid         The n x m grid to visualize.
+     * @param visited_grid The accompanying n x m grid with cells visited information.
+     */
+    public static void printVisitedGrid(ArrayList<ArrayList<Integer>> grid, ArrayList<ArrayList<Boolean>> visited_grid) {
+        // ASCII colors for visuals
+        final String RESET = "\u001B[0m";
+        final String GREEN = "\u001B[32m";
+        final String BLUE = "\u001B[34m";
+
+        // Iterate over entire grid
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid.getFirst().size(); j++) {
+                String cell;
+                // If the number is positive, mark as green ascii box
+                if (grid.get(i).get(j) > 0)
+                    cell = GREEN + "█" + RESET;
+                // If the number is visited, mark as blue ascii box
+                else if (visited_grid.get(i).get(j))
+                    cell = BLUE + "█" + RESET;
+                // Otherwise, mark as dotted box for contrast
+                else
+                    cell = "░";
+                // Print cell with 2 gap spaces to look better
+                System.out.print(cell + "  ");
+            }
+            // Print newline for next row
+            System.out.println();
+        }
     }
 }
